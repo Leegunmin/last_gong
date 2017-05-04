21304c0cebb06132e022858b2a0abd34a4761813
ghrud0520@naver.com <ghrud0520@naver.com>
2017-05-04 14:21:22 +0900

    후기

---
+++ app/src/main/java/nongsa/agoto/loginandregistration/activity/EvalWriteActivity.java
@@ -0,0 +1,15 @@
+package nongsa.agoto.loginandregistration.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import nongsa.agoto.R;
+
+public class EvalWriteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eval_write);
+    }
+}
---
+++ app/src/main/java/nongsa/agoto/loginandregistration/activity/MemberDataAdapter_eval.java
@@ -0,0 +1,8 @@
+package nongsa.agoto.loginandregistration.activity;
+
+/**
+ * Created by Seong Ho-Kyeong on 2017-05-04.
+ */
+
+public class MemberDataAdapter_eval {
+}
